9a4c1470539e17e17d054c83ac81d5db91cbf87c
Joel Cooper <joel@firehawk.systems>
2025-08-29 13:33:23 +1000

    Replace is empty and is not empty to avoid stops, remove rootVars

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ import { trackTwigAccess } from "./tracker.js";
 
 const { twig } = pkg;
 
-const templateString = fs.readFileSync("./template.twig", "utf-8");
+let templateString = fs.readFileSync("./test.twig", "utf-8");
+templateString = templateString.replace(/ is(?: not)? empty(?= )/g, '');
+console.log(templateString)
 const twigTemplate = twig({ data: templateString });
 
-// Load a test client JSON as rootVars
-const rootVars = JSON.parse(fs.readFileSync("./testClient.json", "utf-8"));
-
 // Run with seeded vars
-const usedPaths = trackTwigAccess(vars => twigTemplate.render(vars), rootVars);
+const usedPaths = trackTwigAccess(vars => twigTemplate.render(vars));
 
 console.log("Variables used in template:", usedPaths);
