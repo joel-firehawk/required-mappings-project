d85b63723fdc296a0bf73539a21249493e30e0c5
Joel Cooper <joel@firehawk.systems>
2025-09-10 15:52:29 +1000

    updated comment

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ templateString = templateString.replace(/{%-?\s*endfor\s*-?%}/g, '');
 console.log(templateString)
 const twigTemplate = twig({ data: templateString });
 
-// Run with seeded vars
 const usedPaths = trackTwigAccess(vars => twigTemplate.render(vars));
 
 console.log("Variables used in template:", usedPaths);
